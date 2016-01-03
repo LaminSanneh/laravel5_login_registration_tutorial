6a7dc602718bd432248edc605f9bab8aaff6524f
Lamin Sanneh <lamin.evra@gmail.com>
2016-01-03 11:22:49 +0000

    Final Commit

--- app/User.php
+++ app/User.php
@@ -23,4 +23,15 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public static $login_validation_rules = [
+      'email' => 'required|email|exists:users',
+      'password' => 'required'
+    ];
+
+    public static $create_validation_rules = [
+      'name' => 'required|unique:users',
+      'email' => 'required|email|unique:users',
+      'password' => 'required'
+    ];
 }
